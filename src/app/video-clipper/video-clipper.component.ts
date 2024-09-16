773a4aa6fe4a9dfa16ed0a684c97a2e66dab610c
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-09-15 20:43:18 -0700

    add video-clipper component

---
+++ src/app/video-clipper/video-clipper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-clipper',
+  standalone: true,
+  imports: [],
+  templateUrl: './video-clipper.component.html',
+  styleUrl: './video-clipper.component.scss'
+})
+export class VideoClipperComponent {
+
+}
