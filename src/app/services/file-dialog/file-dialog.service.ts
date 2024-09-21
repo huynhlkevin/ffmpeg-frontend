2535996cd3021e7312c800573c4800f87092fb95
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-09-20 19:00:11 -0700

    add space

--- src/app/services/file-dialog/file-dialog.service.ts
+++ src/app/services/file-dialog/file-dialog.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FileDialogService {
+
   async showOpenDialog(filters: FileFilter[]): Promise<string> {
     // @ts-expect-error Accessing global variables
     return await window.electron.showOpenFileDialog([filters]);
