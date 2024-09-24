e7fcc041424af8eb557002ca267a2a76ecfef586
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-09-23 22:14:43 -0700

    refactor clips

--- src/app/clips/clips.component.ts
+++ src/app/clips/clips.component.ts
@@ -1,29 +1,60 @@
-import { Component } from '@angular/core';
-import { ClipComponent } from './clip/clip.component';
-import { Clip, ClipsService } from '../services/clips/clips.service';
+import { Component, OnInit } from '@angular/core';
+import { ControlContainer, FormArray, FormBuilder, FormControl, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
+import { FilesystemService } from '../services/filesystem/filesystem.service';
 
 @Component({
   selector: 'app-clips',
   standalone: true,
-  imports: [ClipComponent],
+  imports: [ReactiveFormsModule],
   templateUrl: './clips.component.html',
-  styleUrl: './clips.component.scss'
+  styleUrl: './clips.component.scss',
+  viewProviders: [{ provide: ControlContainer, useExisting: FormGroupDirective }]
 })
-export class ClipsComponent {
+export class ClipsComponent implements OnInit {
 
-  clips: Clip[];
+  constructor(
+    private readonly formGroupDirective: FormGroupDirective,
+    private readonly formBuilder: FormBuilder,
+    private readonly filesystemService: FilesystemService
+  ) {
 
-  constructor(private readonly clipsService: ClipsService) {
-    this.clips = clipsService.clips;
   }
 
-  createClip(): void {
-    this.clipsService.createClip();
+  ngOnInit(): void {
+    const clips = this.formBuilder.array([]);
+    this.formGroupDirective.form.addControl('clips', clips);
   }
 
-  deleteClip(index: number): void {
-    const clip = this.clipsService.clips[index];
-    this.clipsService.deleteClip(clip);
-    this.clips = this.clipsService.clips;
+  onCreateClipClick(): void {
+    const clip = this.formBuilder.group({
+      sourceVideoFile: [''],
+      startTime: [''],
+      endTime: ['']
+    });
+    this.clips.push(clip);
+    console.log(this.formGroupDirective.form.get('clips'));
+  }
+
+  onDeleteClipClick(index: number): void {
+    console.log(this.clips);
+    this.clips.removeAt(index);
+  }
+
+  get clips(): FormArray {
+    return this.formGroupDirective.form.get('clips') as FormArray;
+  }
+
+  async onSelectSourceVideoFileClick(index: number): Promise<void> {
+    const extensions = ['mp4', 'mkv']
+    const file = await this.filesystemService.showOpenFileDialog([
+      {
+        extensions,
+        name: `Video Files (${extensions.map(extension => `*.${extension}`)})`
+      }
+    ]);
+    if (file) {
+      const sourceVideoFile = this.clips.get(`${index}.sourceVideoFile`) as FormControl;
+      sourceVideoFile.setValue(file);
+    }
   }
 }
