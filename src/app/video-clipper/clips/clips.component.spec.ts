d9a3e094d3bd53777d6fecb2ce478512aea64bef
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-09-24 17:32:18 -0700

    fix test

--- src/app/video-clipper/clips/clips.component.spec.ts
+++ src/app/video-clipper/clips/clips.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClipsComponent } from './clips.component';
+import { FormGroup, FormGroupDirective } from '@angular/forms';
 
 describe('ClipsComponent', () => {
   let component: ClipsComponent;
   let fixture: ComponentFixture<ClipsComponent>;
+  let formGroupDirective: FormGroupDirective;
 
   beforeEach(async () => {
+    formGroupDirective = new FormGroupDirective([], []);
+    formGroupDirective.form = new FormGroup({});
     await TestBed.configureTestingModule({
-      imports: [ClipsComponent]
+      imports: [ClipsComponent],
+      providers: [{ provide: FormGroupDirective, useValue: formGroupDirective }]
     })
     .compileComponents();
 
@@ -19,5 +24,9 @@ describe('ClipsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have add codec to form', () => {
+    expect(formGroupDirective.form.get('clips')).toBeTruthy();
   });
 });
