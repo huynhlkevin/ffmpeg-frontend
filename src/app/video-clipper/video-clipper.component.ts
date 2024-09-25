a579ccbb6862da3148ba340665f0a63cc9b4cb62
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-09-24 17:22:45 -0700

    fix import, minor changes

--- src/app/video-clipper/video-clipper.component.ts
+++ src/app/video-clipper/video-clipper.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { InputDirectoryComponent } from "./input-directory/input-directory.component";
 import { VideoEncodingComponent } from './video-encoding/video-encoding.component';
 import { AudioEncodingComponent } from './audio-encoding/audio-encoding.component';
-import { ClipsComponent } from '../clips/clips.component';
+import { ClipsComponent } from './clips/clips.component';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -27,6 +27,6 @@ export class VideoClipperComponent {
   }
 
   onSubmit(): void {
-    console.log(this.videoClipperForm.valid);
+    console.log(this.videoClipperForm);
   }
 }
