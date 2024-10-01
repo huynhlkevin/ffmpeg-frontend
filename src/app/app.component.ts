cde3d28705cfd5affe27d38f56e8888e5a1ae7ca
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-09-30 18:06:27 -0700

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { VideoClipperComponent } from "./video-clipper/video-clipper.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, VideoClipperComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, VideoClipperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
