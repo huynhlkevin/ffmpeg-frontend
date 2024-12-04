1d445ddcae13f81590bd40e956b89e19dd65f502
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-03 20:16:29 -0800

    Fix NullInjectorError

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, RouterModule.forRoot(routes)]
     }).compileComponents();
   });
 
