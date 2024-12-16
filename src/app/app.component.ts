befd25f235c4dc55d074b40f2976d77dff4ef721
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-15 21:54:34 -0800

    edit title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { VideoClipperComponent } from "./video-clipper/video-clipper.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, RouterLink, RouterLinkActive, VideoClipperComponent],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ffmpeg-frontend';
+  title = 'FFMPEG Frontend';
 }
